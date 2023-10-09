8377370cf37aaa1c6c26b6bd7f8209128536dfc2
Owner PC <PC-Owner-t@outlook.jp>
2023-10-09 16:56:57 +0900

    プロジェクト ファイルを追加します。

---
+++ 2023_10_03_Task_2/2023_10_03_Task_2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+static int Mult(int num1, int num2) {
+    return num1 * num2;
+}
+
+int main()
+{
+    int num1 = 0, num2 = 0;
+
+    printf("値1を入力 > ");
+    cin >> num1;
+    printf("値2を入力 > ");
+    cin >> num2;
+
+    printf("%d * %d = %d\n", num1, num2, Mult(num1, num2));
+}
